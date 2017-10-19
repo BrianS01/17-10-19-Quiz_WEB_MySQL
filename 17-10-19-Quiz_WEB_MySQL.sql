4a9bfdb9034e6a6e7fb3bba7d7d864d8fb4abf08
BrianS01 <brian.sterling@correo.usa.edu.co>
2017-10-19 09:55:24 -0500

    Quiz

--- 17-10-19-Quiz_WEB_MySQL.sql
+++ 17-10-19-Quiz_WEB_MySQL.sql
@@ -4,42 +4,35 @@ use BasesQuizEsquema;
 
 CREATE TABLE EsqBaseD.`Squemas`
 (
-  `Squemaid` int(11) NOT NULL AUTO_INCREMENT,
-  `firstname` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`Squemaid`)
+	`Squemaid` int(11) NOT NULL AUTO_INCREMENT,
+	`firstname` varchar(45) DEFAULT NULL,
+	PRIMARY KEY(`Squemaid`)
 );
 
 
 CREATE TABLE EsqBaseD.`tabla`
 (
-  `tablaid` int(11) NOT NULL AUTO_INCREMENT,
-  `esquemaid` int(11) NOT null,
-  `firstname` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`tablaid`),
-	FOREIGN KEY (esquemaid) REFERENCES squemas(Squemaid)
+	`tablaid` int(11) NOT NULL AUTO_INCREMENT,
+	`esquemaid` int(11) NOT null,
+	`firstname` varchar(45) DEFAULT NULL,
+	PRIMARY KEY(`tablaid`),
+	FOREIGN KEY(esquemaid) REFERENCES squemas(Squemaid)
 );
+
 
 CREATE TABLE EsqBaseD.`columnas`
 (
-  `columnaid` int(11) NOT NULL AUTO_INCREMENT,
-  `tablaid1` int(11) NOT null,
-  `firstname` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`columnaid`),
-  
-
-  
-  
-FOREIGN KEY (tablaid1) REFERENCES tabla(tablaid)
+	`columnaid` int(11) NOT NULL AUTO_INCREMENT,
+	`tablaid1` int(11) NOT null,
+	`firstname` varchar(45) DEFAULT NULL,
+	PRIMARY KEY (`columnaid`),
+    FOREIGN KEY (tablaid1) REFERENCES tabla(tablaid)
 );
 
-CREATE TABLE EsqBaseD.`FOREIGN KEY` (
-  `forgeinid` int(11) NOT NULL AUTO_INCREMENT,
-  
-  `firstname` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`forgeinid`)
-  
 
-  
-  
+CREATE TABLE EsqBaseD.`FOREIGN KEY`
+(
+	`forgeinid` int(11) NOT NULL AUTO_INCREMENT,
+    `firstname` varchar(45) DEFAULT NULL,
+    PRIMARY KEY (`forgeinid`)
 );
-
