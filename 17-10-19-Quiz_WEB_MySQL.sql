48b7b3c65fb0b50f0a4fde11e19cd84743ea4f54
BrianS01 <brian.sterling@correo.usa.edu.co>
2017-10-19 10:15:32 -0500

    Quiz

--- 17-10-19-Quiz_WEB_MySQL.sql
+++ 17-10-19-Quiz_WEB_MySQL.sql
@@ -65,37 +65,20 @@ create table BasesQuizEsquema.`Modelo`
 
 
 insert into squemas(Squemaid, firstname) values(1, "primero");
-
-
 insert into Tabla(tablaid, esquemaid, firstname) values(1,1, "segundo");
+insert into Columnas(columnaid, tablaid1, firstname) values(1, 1, "tercero");
+
+
 
 
 insert into () values();
-CREATE TABLE BasesQuizEsquema.`Columnas`
-(
-	`columnaid` int(11) NOT NULL AUTO_INCREMENT,
-	`tablaid1` int(11) NOT null,
-	`firstname` varchar(45) DEFAULT NULL,
-	PRIMARY KEY (`columnaid`),
-    FOREIGN KEY (tablaid1) REFERENCES tabla(tablaid)
-);
-
-insert into () values();
-CREATE TABLE BasesQuizEsquema.`FOREIGN KEY`
-(
-	`forgeinid` int(11) NOT NULL AUTO_INCREMENT,
-    `firstname` varchar(45) DEFAULT NULL,
-    PRIMARY KEY (`forgeinid`)
-);
-
-
 create table BasesQuizEsquema.`Unidad`
 (
 	`idUnidad` int(11) NOT NULL AUTO_INCREMENT,
     primary key(`idUnidad`)
 );
 
-
+insert into () values();
 create table BasesQuizEsquema.`Contexto`
 (
 	`idContexto` int(11) NOT NULL AUTO_INCREMENT,
@@ -103,7 +86,7 @@ create table BasesQuizEsquema.`Contexto`
     primary key(`idContexto`),
     foreign key(idUnidad) references unidad(idUnidad)
 );
-
+insert into () values();
 
 create table BasesQuizEsquema.`Modelo`
 (
