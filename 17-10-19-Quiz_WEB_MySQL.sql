39f8521e6b7f254f0c8697553af49fcafaed2745
BrianS01 <brian.sterling@correo.usa.edu.co>
2017-10-19 10:13:51 -0500

    Quiz

--- 17-10-19-Quiz_WEB_MySQL.sql
+++ 17-10-19-Quiz_WEB_MySQL.sql
@@ -49,6 +49,7 @@ create table BasesQuizEsquema.`Contexto`
 (
 	`idContexto` int(11) NOT NULL AUTO_INCREMENT,
     `idUnidad` int(11) NOT NULL,
+    `link` varchar(11) NOT NULL,
     primary key(`idContexto`),
     foreign key(idUnidad) references unidad(idUnidad)
 );
@@ -63,9 +64,51 @@ create table BasesQuizEsquema.`Modelo`
 );
 
 
-insert into squemas
-INSERT INTO Ciudad (
-Id_Ciudad,
-Nom_Ciudad
-)
-VALUES (1 ,"Cartagena"),
+insert into squemas(Squemaid, firstname) values(1, "primero");
+
+
+insert into Tabla(tablaid, esquemaid, firstname) values(1,1, "segundo");
+
+
+insert into () values();
+CREATE TABLE BasesQuizEsquema.`Columnas`
+(
+	`columnaid` int(11) NOT NULL AUTO_INCREMENT,
+	`tablaid1` int(11) NOT null,
+	`firstname` varchar(45) DEFAULT NULL,
+	PRIMARY KEY (`columnaid`),
+    FOREIGN KEY (tablaid1) REFERENCES tabla(tablaid)
+);
+
+insert into () values();
+CREATE TABLE BasesQuizEsquema.`FOREIGN KEY`
+(
+	`forgeinid` int(11) NOT NULL AUTO_INCREMENT,
+    `firstname` varchar(45) DEFAULT NULL,
+    PRIMARY KEY (`forgeinid`)
+);
+
+
+create table BasesQuizEsquema.`Unidad`
+(
+	`idUnidad` int(11) NOT NULL AUTO_INCREMENT,
+    primary key(`idUnidad`)
+);
+
+
+create table BasesQuizEsquema.`Contexto`
+(
+	`idContexto` int(11) NOT NULL AUTO_INCREMENT,
+    `idUnidad` int(11) NOT NULL,
+    primary key(`idContexto`),
+    foreign key(idUnidad) references unidad(idUnidad)
+);
+
+
+create table BasesQuizEsquema.`Modelo`
+(
+	`idModelo` int(11) NOT NULL AUTO_INCREMENT,
+    `idUnidad` int(11) NOT NULL,
+    primary key(`idModelo`),
+    foreign key(idUnidad) references unidad(idUnidad)
+);
