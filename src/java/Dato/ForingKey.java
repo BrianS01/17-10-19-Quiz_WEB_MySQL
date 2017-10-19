ab069f1e40a5f3fbba2f1d0f6d201f1a208de368
BrianS01 <brian.sterling@correo.usa.edu.co>
2017-10-19 10:57:50 -0500

    Quiz

--- src/java/Dato/ForingKey.java
+++ src/java/Dato/ForingKey.java
@@ -1,23 +1,22 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *  Quiz WEB MySQL
+ *      Author ::: Brian Sterling
+ *     Program ::: Bases de Datos
+ *  Credential ::: SIST0008-G01:SIV
  */
 package Dato;
 
-/**
- *
- * @author Mario-Bx
- */
-public class ForingKey extends Columna {
-
+public class ForingKey extends Columna 
+{
     private Tabla id_ForingKey;
 
-    public ForingKey(Tabla id_ForingKey) {
+    public ForingKey(Tabla id_ForingKey)
+    {
         this.id_ForingKey = id_ForingKey;
     }
 
-    public ForingKey(int id_Colimna, String Nombre, String tipoDato) {
+    public ForingKey(int id_Colimna, String Nombre, String tipoDato)
+    {
         super(id_Colimna, Nombre, tipoDato);
     }
 }
--- src/java/Dato/Squema.java
+++ src/java/Dato/Squema.java
@@ -1,16 +1,13 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *  Quiz WEB MySQL
+ *      Author ::: Brian Sterling
+ *     Program ::: Bases de Datos
+ *  Credential ::: SIST0008-G01:SIV
  */
 package Dato;
 
 import java.util.*;
 
-/**
- *
- * @author Mario-Bx
- */
 public class Squema {
 
     private int id_Squema;
